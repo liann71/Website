0b795313599ad484f237118552669182724cd5aa
lian <lian@penguin.com>
2024-02-26 09:57:51 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+echo "hello world!<br>"; 
+
+$name = "Jullian";
+$age = "20";
+$email = "dorero74@gmail.com";
+$user = "2";
+$x = 10;
+$y = 2;
+$z = null;
+$z = $x+$y;
+
+echo"hello, {$name}. ";
+echo"welcome to the website.<br>";
+echo"your age is: {$age}<br>";
+echo"email: <a> {$email}</a><br><br>";
+echo"online users: {$user}<br><br>";
+echo"answer: {$z}";
+?>
+<br>
+<a href="https://www.youtube.com/">Login</a>
